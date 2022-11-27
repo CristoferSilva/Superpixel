7d47ee1bfb914632f700cea9db644528aabb2240
CristoferSilva <gabrielc2828@gmail.com>
2022-11-27 18:15:03 -0300

    [REFACT]

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from graph.Graph import Graph
-from graph.Key import Key
-from graph.Vertex import Vertex
-import numpy as np
-#my_graph = Vertex(Key(1, 1), 0)
+
 my_graph = Graph("images/car.jpg", 100)
 my_graph.showImageWithSuperpixels()
