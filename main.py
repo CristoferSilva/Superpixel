2b6ee1b040243496a3d7ce86f95e1d197c47370b
CristoferSilva <gabrielc2828@gmail.com>
2022-11-27 20:48:30 -0300

    [REFACT]

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from graph.Graph import Graph
 
-my_graph = Graph("images/simpleColorImages0.jpg", 2)
+my_graph = Graph("images/simpleColorImages0.jpg", 3)
 my_graph.showImageWithSuperpixels()
 
 
